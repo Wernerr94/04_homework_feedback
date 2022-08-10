403c721d04e189c382be71860429f84d14ffa322
Oleksander Verner <alexanderwerner942015@gmail.com>
2022-08-10 13:37:31 +0200

    deploy

--- src/components/ImageFinderOnHOOK/Modal/Modal.jsx
+++ src/components/ImageFinderOnHOOK/Modal/Modal.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal({ onClose, children }) {
+  const [keyCode, setKeyCode] = useState('');
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
+      setKeyCode('Escape');
       onClose();
     }
   };
@@ -21,7 +23,7 @@ export default function Modal({ onClose, children }) {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [keyCode]);
 
   return createPortal(
     <div className={css.overlay} onClick={handleClosing}>
